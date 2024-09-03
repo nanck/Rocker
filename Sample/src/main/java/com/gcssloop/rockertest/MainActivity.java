322f61f96de94084ddbc07937a807645973027d1
田建国 <tjg356@163.com>
2024-09-03 11:14:35 +0800

    chore: 加入方向

--- Sample/src/main/java/com/gcssloop/rockertest/MainActivity.java
+++ Sample/src/main/java/com/gcssloop/rockertest/MainActivity.java
@@ -21,19 +21,18 @@ public class MainActivity extends AppCompatActivity {
         try {
             RockerView rocker = (RockerView) findViewById(R.id.rocker);
             if (null != rocker) {
-                rocker.setListener(new RockerView.RockerListener() {
+                rocker.setListener((eventType, currentAngle, currentDistance, currentState) -> {
+                    float d1 = currentState.data1;
+                    float d2 = currentState.data2;
 
-                    @Override
-                    public void callback(int eventType, int currentAngle, float currentDistance, RockerView.State currentState) {
-                        switch (eventType) {
-                            case RockerView.EVENT_ACTION:
-                            case RockerView.EVENT_CLOCK:
-                                // 定时回调
-                                // 触摸事件回调
-                                Log.e("EVENT_ACTION-------->", "angle=" + currentAngle + " - distance" + currentDistance + " - state " + currentState.text);
-                                eachDebug(eventType, currentAngle, currentDistance, currentState);
-                                break;
-                        }
+                    switch (eventType) {
+                        case RockerView.EVENT_ACTION:
+                        case RockerView.EVENT_CLOCK:
+                            // 定时回调
+                            // 触摸事件回调
+                            Log.e("EVENT_ACTION-------->", "angle=" + currentAngle + " - distance" + currentDistance + " - state " + currentState.text);
+                            eachDebug(eventType, currentAngle, currentDistance, currentState);
+                            break;
                     }
                 });
             }
@@ -43,11 +42,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void eachDebug(int eventType, int currentAngle, float currentDistance, RockerView.State currentState) {
+        String direction = angleToDirection(currentAngle);
         String info = String.format(Locale.getDefault(),
-                "debug\n事件类型: %d\n角度: $%d\n距离: %f\n状态:%s\ndata1: %.2f\ndata2: %.2f",
-                eventType, currentAngle, currentDistance, currentState.text, currentState.data1, currentState.data2);
+                "debug\n事件类型: %d\n角度: $%d\n方向: %s\n距离: %f\n状态:%s\ndata1: %.2f\ndata2: %.2f",
+                eventType, currentAngle, direction, currentDistance, currentState.text, currentState.data1, currentState.data2);
         tvDebug.post(() -> {
             tvDebug.setText(info);
         });
+    }
+
+
+    public static String angleToDirection(double angle) {
+        String[] directions = {"东", "东北", "北", "西北", "西", "西南", "南", "东南"};
+        int index = (int) ((angle % 360) / 45);
+        return directions[index];
     }
 }
